175c0274b610d2794fc7782a58545d01e3dcf3ef
Sarah Eichenstein <sarahy97@gmail.com>
2018-01-16 19:01:58 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,79 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = ""
+  data.each do |s, array|
+    if s == season
+      array.each do |contestant|
+        contestant.each do |k,v|
+          if v == "Winner"
+            name = contestant["name"]
+            name = name.split(" ").shift
+            winner << name
+          end
+        end
+      end
+    end
+  end
+  winner
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = ""
+  data.each do |season, array|
+    array.each do |contestant|
+      contestant.each do |k, v|
+        if v == occupation
+          name << contestant["name"]
+        end
+      end
+    end
+  end
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, array|
+    array.each do |contestant|
+      contestant.each do |k, v|
+        if v == hometown
+          count +=1
+        end
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  job = []
+  data.each do |season, array|
+    array.each do |contestant|
+      contestant.each do |k,v|
+        if v == hometown
+          job << contestant["occupation"]
+        end
+      end
+    end
+  end
+  job = job.shift
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  sum = 0
+  data.each do |s, a|
+    if s == season
+      a.each do |contestant|
+        ages << contestant["age"]
+      end
+    end
+  end
+  ages.each do |n|
+    n = n.to_f
+    sum += n
+  end
+  avg = sum / ages.length
+  avg.round
 end
